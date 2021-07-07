0ece516b586f13a119b1721e3ac583879b856e93
AlbanDAQUINO <1471248+AlbanDAQUINO@users.noreply.github.com>
2021-07-07 11:55:34 +0200

    Initial Commit

---
+++ RSi Launcher.cmd
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+SETLOCAL ENABLEEXTENSIONS
+SET _ME=%~n0
+SET _PARENT=%~dp0
+
+SET INSTALL_LOCATION=C:\Games\Roberts Space Industries
+
+
+ECHO Ultimate Shaders/USER directory remover for Star Citizen
+ECHO --------------------------------------------------------
+ECHO - v0.1 (AlexGranger)
+ECHO.
+
+ECHO ===== LIVE Environment :
+IF EXIST "%INSTALL_LOCATION%\StarCitizen\LIVE\USER\Client\0\Shaders" (
+  ECHO Removing Shaders in the LIVE Environment !
+  RD /S /Q "%INSTALL_LOCATION%\StarCitizen\LIVE\USER\Client\0\Shaders"
+  ) ELSE (
+  ECHO No files found for the LIVE Environment ...
+  )
+TIMEOUT /T 5
+ECHO.
+
+ECHO ===== PTU Environment :
+IF EXIST "%INSTALL_LOCATION%\StarCitizen\PTU\USER\Client\0\Shaders" (
+  ECHO Removing Shaders in the PTU Environment !
+  RD /S /Q "%INSTALL_LOCATION%\StarCitizen\PTU\USER\Client\0\Shaders"
+  ) ELSE (
+  ECHO No files found for the PTU Environment ...
+  )
+TIMEOUT /T 5
+ECHO.
+
+
+
+ECHO Starting Launcher ...
+START /ABOVENORMAL "Star Citizen" /D "%INSTALL_LOCATION%\RSI Launcher" "RSI Launcher.exe"
+ECHO.
+
+#PAUSE
