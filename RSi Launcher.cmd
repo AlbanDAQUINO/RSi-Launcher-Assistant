b756bda0ee354022efb42da969d1e9ed71ad8285
Alban d'Aquino <alban.daquino@gmail.com>
2021-07-10 15:04:48 +0200

    Update: Minor update, adding USER Key Binding feature (v0.3.1)

--- RSi Launcher.cmd
+++ RSi Launcher.cmd
@@ -1,41 +1,63 @@
 @ECHO OFF
 
-SETLOCAL ENABLEEXTENSIONS
-SET _ME=%~n0
-SET _PARENT=%~dp0
-
 SET INSTALL_LOCATION=C:\Games\Roberts Space Industries
 
 
-ECHO Ultimate Shaders/USER directory remover for Star Citizen
+ECHO.
+ECHO Ultimate Shaders/USER directory cleaner for Star Citizen
 ECHO --------------------------------------------------------
-ECHO - v0.2 (AlexGranger)
+ECHO - v0.3.1 (AlexGranger)
 ECHO.
 
-ECHO ===== LIVE Environment :
+
+
+ECHO LIVE Environment
+ECHO -----------------
+IF EXIST "%INSTALL_LOCATION%\StarCitizen\LIVE\USER\Client\0\Profiles\default\actionmaps.xml" (
+  ECHO -- Key Bindings - Detected ^!
+  ECHO  ^|-+ Creating backup ...
+  ECHO  ^|-- Cleaning directory ^!
+  ECHO  --+ Restoring backup ^!
+  ) ELSE (
+  ECHO -- Key Bindings - No file found ...
+  ECHO  ^|-+ Restoring backup ^!
+  )
 IF EXIST "%INSTALL_LOCATION%\StarCitizen\LIVE\USER\Client\0\Shaders" (
-  ECHO Removing Shaders !
+  ECHO -- Shaders - Detected ^!
+  ECHO  ^|-- Cleaning directory ^!
   RD /S /Q "%INSTALL_LOCATION%\StarCitizen\LIVE\USER\Client\0\Shaders"
   ) ELSE (
-  ECHO No files found ...
+  ECHO -- Shaders - No files found ...
   )
 TIMEOUT /T 5
 ECHO.
 
-ECHO ===== PTU Environment :
+
+
+ECHO PTU Environment
+ECHO ----------------
+IF EXIST "%INSTALL_LOCATION%\StarCitizen\PTU\USER\Client\0\Profiles\default\actionmaps.xml" (
+  ECHO -- Key Bindings - Detected ^!
+  ECHO  ^|-+ Creating backup ...
+  ECHO  ^|-- Cleaning directory ^!
+  ECHO  --+ Restoring backup ^!
+  ) ELSE (
+  ECHO -- Key Bindings - No file found ...
+  ECHO  ^|-+ Restoring backup ^!
+  )
 IF EXIST "%INSTALL_LOCATION%\StarCitizen\PTU\USER\Client\0\Shaders" (
-  ECHO Removing Shaders !
+  ECHO -- Shaders - Detected ^!
+  ECHO  ^|-- Cleaning directory ^!
   RD /S /Q "%INSTALL_LOCATION%\StarCitizen\PTU\USER\Client\0\Shaders"
   ) ELSE (
-  ECHO No files found ...
+  ECHO -- Shaders - No files found ...
   )
 TIMEOUT /T 5
 ECHO.
 
 
 
-ECHO Starting Launcher ...
+ECHO Starting Launcher -------------------------------------
+ECHO -- Starting Process with ABOVENORMAL priority ...
 START /ABOVENORMAL "Star Citizen" /D "%INSTALL_LOCATION%\RSI Launcher" "RSI Launcher.exe"
 ECHO.
-
-#PAUSE
